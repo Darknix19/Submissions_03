61715e53d0a0f944ca207de592455273b88908cd
Khunais Baig <khunaisbaig29@gmail.com>
2025-11-21 23:07:38 +0500

    Update First Palindromic String in the Array.c

--- First Palindromic String in the Array.c
+++ First Palindromic String in the Array.c
@@ -8,26 +8,27 @@ int isPalindrome(char *s) {
         left++;
         right--;
     }
-  
     return 1;
 }
 
 int main() {
     int n;
+    printf("Enter the number of words: ");
     scanf("%d", &n);
+
     char words[105][105];
+    printf("Enter each word (one per line):\n");
     for (int i = 0; i < n; i++) {
         scanf("%s", words[i]);
     }
 
     for (int i = 0; i < n; i++) {
         if (isPalindrome(words[i])) {
-            printf("%s\n", words[i]);
+            printf("First palindromic word: %s\n", words[i]);
             return 0;
         }
     }
 
-    printf("\n");
-  
+    printf("No palindromic word found.\n");
     return 0;
 }
